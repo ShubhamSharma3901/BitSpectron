787df1626e0d748de70888ac525e93f172932fbd
Shubham Sharma <shubham03901@gmail.com>
2024-04-12 20:47:16 +0530

    v1.0

--- app/api/resend-email/route.ts
+++ app/api/resend-email/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
